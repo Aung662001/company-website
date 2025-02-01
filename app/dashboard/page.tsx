0159d4b3924257fd53ffb6198032adbc7e6aec07
Aung662001 <setannatsoegodzzz@gmail.com>
2025-02-02 05:58:38 +0630

    builded

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,21 @@
-"use client"
-import React ,{useState} from "react";
-import {  RootState } from "@/context/ConfigureStore";
+"use client";
+import React, { useState } from "react";
+import { RootState } from "@/context/ConfigureStore";
 import { redirect } from "next/navigation";
 import { useSelector } from "react-redux";
 
 const page = () => {
-  const { isLogin,role } = useSelector((state: RootState) => state.auth);
-  const [orders,setOrders] = useState([])
-//   if (!isLogin) {
-//     redirect("/login");
-//   }
-  return <div className="flex flex-col justify-center items-center">
-    <p className="font-3xl text-slate-600 text-center my-4">Orders </p></div>;
+  const { isLogin, role } = useSelector((state: RootState) => state.auth);
+  const [orders, setOrders] = useState([]);
+  //   if (!isLogin) {
+  //     redirect("/login");
+  //   }
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <p className="font-3xl text-slate-600 text-center my-4">Orders </p>
+      <div>{orders && orders.map((order, i) => <div key={i}></div>)}</div>
+    </div>
+  );
 };
 
 export default page;
