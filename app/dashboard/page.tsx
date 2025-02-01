ef0bd7c417755e8a32aaabacd7451a3f10edb528
Aung662001 <setannatsoegodzzz@gmail.com>
2025-02-02 06:00:46 +0630

    builded

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,17 +3,20 @@ import React, { useState } from "react";
 import { RootState } from "@/context/ConfigureStore";
 import { redirect } from "next/navigation";
 import { useSelector } from "react-redux";
+import { OrderFormData } from "@/utils/types";
 
 const page = () => {
   const { isLogin, role } = useSelector((state: RootState) => state.auth);
-  const [orders, setOrders] = useState([]);
+  const [orders, setOrders] = useState<OrderFormData[]>([]);
   //   if (!isLogin) {
   //     redirect("/login");
   //   }
   return (
     <div className="flex flex-col justify-center items-center">
       <p className="font-3xl text-slate-600 text-center my-4">Orders </p>
-      <div>{orders && orders.map((order, i) => <div key={i}></div>)}</div>
+      <div>{orders && orders.map((order, i) => <div key={i}>
+        <p>{order.hospital_name}</p>
+      </div>)}</div>
     </div>
   );
 };
